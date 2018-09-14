119f062e19200c4d341f9329b2ef8a45cf69e8e8
andriusbaltrunas <andriusbaltrunas@yahoo.com>
2018-09-14 19:10:09 +0300

    trecia

--- 1_diena/src/days/second/ThirdExample.java
+++ 1_diena/src/days/second/ThirdExample.java
@@ -1,19 +1,33 @@
 package days.second;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ThirdExample {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Iveskite ugi");
-        double ugis = sc.nextDouble();
-        System.out.println("Iveskite svori");
-        double svoris = sc.nextDouble();
+        double ugis = getCorrectNumb(sc, "Iveskite ugi");
+        double svoris = getCorrectNumb(sc, "Iveskite svori");
         System.out.println("KMI YRA " + kmi(ugis, svoris));
     }
 
-    private static double kmi(double ugis, double svoris){
-        double kmi = svoris/Math.pow(ugis, 2);
+    private static double kmi(double ugis, double svoris) {
+        double kmi = svoris / Math.pow(ugis, 2);
         return kmi;
+    }
+
+    private static double getCorrectNumb(Scanner sc, String text) {
+        System.out.println(text);
+        double val = 0;
+        while (true) {
+            try {
+                val = sc.nextDouble();
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("Blogai ivestas skaicius pakartokite");
+                sc.nextLine();
+            }
+        }
+        return val;
     }
 }
